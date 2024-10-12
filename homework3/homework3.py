e757d37065e67ae9e85fef27c35f74cc82bd8446
MEIMAL <131466033+MEIMAL@users.noreply.github.com>
2024-10-12 11:51:49 +0500

    Create homework3.py

---
+++ homework3/homework3.py
@@ -0,0 +1,15 @@
+# Цикл для ввода чисел
+while True:
+    user_input = input("Введите число (или 'end' для завершения): ")
+    if user_input.lower() == 'end':
+        break
+    try:
+        # Преобразование введенного значения в целое число
+        number = int(user_input)
+        numbers.append(number)  # Добавление числа в список
+    except ValueError:
+        print("Ошибка: введите корректное целое число.")
+
+# Вывод нечетных элементов списка
+odd_numbers = [num for num in numbers if num % 2 != 0]
+print("Нечетные элементы списка:", odd_numbers)
